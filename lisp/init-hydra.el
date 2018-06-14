d16b3702fa5fcda15c85a48f9a613e92e22e56ef
ndjuric <metod303@gmail.com>
2018-06-14 18:51:37 +0200

    add hydra bindings

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -12,4 +12,16 @@
                       :foreground "SkyBlue1" :bold t :background "#383838")
   (hydra-add-font-lock))
 
+(defhydra hydra-zoom (global-map "<f2>")
+  "zoom"
+  ("g" text-scale-increase "in")
+  ("l" text-scale-decrease "out"))
+
+(defhydra hydra-splitter (global-map "C-M-s")
+  "splitter"
+  ("h" hydra-move-splitter-left)
+  ("j" hydra-move-splitter-down)
+  ("k" hydra-move-splitter-up)
+  ("l" hydra-move-splitter-right))
+
 (provide 'init-hydra)
