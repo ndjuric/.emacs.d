b6716d9ab6e9717c4ef27195cb089ae2bcd0ddd6
ndjuric <metod303@gmail.com>
2018-06-16 10:07:15 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,45 @@
+;; start
+(defvar gc-cons-threshold--orig gc-cons-threshold)
+(setq gc-cons-threshold (* 100 1024 1024) gc-cons-percentage 0.6)
+
+(defun set-custom-gc-threshold ()
+  (setq gc-cons-threshold gc-cons-threshold--orig gc-cons-percentage 0.1))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+
+(when (not package-archive-contents)
+    (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(add-to-list 'load-path (concat user-emacs-directory "lisp/"))
+
+;; set home and emacs dirs
+(defvar user-home-directory (concat (getenv "HOME") "/"))
+(setq user-emacs-directory (concat user-home-directory ".emacs.d/"))
+
+;; save custom to separate dir
+(setq custom-file (concat user-emacs-directory "custom/settings.el"))
+(load custom-file :noerror :nomessage) ;; silent load
+
+(require 'init-general)
+(require 'init-ivy-counsel)
+(require 'init-helm)
+(require 'init-helm-gtags)
+(require 'init-cedet)
+(require 'init-editing)
+
+(package-install-selected-packages)
+
+(require 'server)
+(unless (server-running-p) (server-start))
+
+;; reset gc-cons-threshold to original
+(add-hook 'emacs-startup-hook #'set-custom-gc-threshold)
