cdb7f1f7dabcb31c6f7d25a31e6388656244af1f
ndjuric <metod303@gmail.com>
2020-05-01 04:26:41 +0200

    remove php from custom settings

--- custom/settings.el
+++ custom/settings.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (company-lsp lsp-ui lsp-mode flycheck phpunit php-mode counsel ivy projectile format-all company beacon dimmer gruvbox-theme rainbow-delimiters key-chord system-packages use-package))))
+   '(company-lsp lsp-ui lsp-mode counsel ivy projectile format-all company beacon dimmer gruvbox-theme rainbow-delimiters key-chord system-packages use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
